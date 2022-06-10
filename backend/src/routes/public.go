56a795d83209c25af31ae36b9714d81dca2130d6
Andrey Shibaev <andrw.shibaev@gmail.com>
2022-06-10 19:45:02 +0200

    login base

--- backend/src/routes/public.go
+++ backend/src/routes/public.go
@@ -12,6 +12,7 @@ func PublicRoutes(a *fiber.App) {
 
 	// Routes for GET method:
 	route.Get("/enterprises", controllers.GetEnterprises)
+	route.Post("/login", controllers.Login)
 	route.Get("/any", func(c *fiber.Ctx) error {
 		return c.SendString("123")
 	})
