ec363f0ea6538ec886aebea4ba36efd3ac30b874
Nikita <1nikita7@mail.ru>
2022-06-13 03:42:37 +0300

    Dragon drop

--- frontend/src/components/UploadTable/UploadTable.jsx
+++ frontend/src/components/UploadTable/UploadTable.jsx
@@ -5,7 +5,9 @@ import * as XLSX from 'xlsx/xlsx.mjs';
 const UploadTable = () => {
   const [items, setItems] = useState([]);
   console.log(items);
-  const readExcel = (file) => {
+  const readExcel = (file, e) => {
+    // eslint-disable-next-line no-unused-expressions
+    e.preventDefault
     const promise = new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsArrayBuffer(file);
@@ -34,13 +36,45 @@ const UploadTable = () => {
     });
   };
   
+  const [drag, setDrag] = useState(false)
+
+  const dragStartHadnler = (e) =>{
+    e.preventDefault()
+    setDrag(true)
+  }
+
+  const dragLeaveHadnler = (e) =>{
+    e.preventDefault()
+    setDrag(false)
+  }
+
+  
   return(
     <div className={style.container}>
-<div>
-<input type='file' accept='.xlsx, .xls' onChange={(e) => {
+<div className={style.upload}>
+      <h2 className={style.h2} >Информация о товаре</h2>
+{/* <input type='file' accept='.xlsx, .xls' onChange={(e) => {
           const file = e.target.files[0];
           readExcel(file);
-        }} />
+        }} /> */}
+{ drag 
+?<div className={style.drop}
+onDragStart={e => dragStartHadnler(e)}
+onDragLeave={e => dragLeaveHadnler(e)}
+onDragOver={e => dragStartHadnler(e)}
+onDrop={e => {
+  const file = e.dataTransfer.files[0];
+  readExcel(file,e);
+}}
+>Отпустите файл xlsx, чтобы загрузить его</div>
+ : <div className={style.drop2}
+ onDragStart={e => dragStartHadnler(e)}
+ onDragLeave={e => dragLeaveHadnler(e)}
+ onDragOver={e => dragStartHadnler(e)}
+ 
+ >Перетащите файл xlsx, чтобы загрузить его</div>
+
+}
 </div>
 <div className={style.desc}>
 <h2 className={style.h2}>Инструкция</h2>
@@ -48,9 +82,6 @@ const UploadTable = () => {
   <p>Скачать пример таблицы можно ниже.</p>
   <button className={style.btn}>Скачать</button>
 </div>
-
-
-
     </div>
 
     
